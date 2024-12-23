be1ed6dabfc85f139ba03d83688f2b1db24ba5a9
Tom Cogzell <tomcogzell8@gmail.com>
2024-12-23 01:02:57 +0000

    Fixed flask connection error

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 
 from flask import render_template
-import connexion
-import models
+import config
+from models import Trail
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
