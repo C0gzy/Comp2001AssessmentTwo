6492f9bfcbabadc05770b72921b619f2ce4769a1
Tom Cogzell <58867012+C0gzy@users.noreply.github.com>
2024-12-24 02:37:41 +0000

    Added User db and forigenkey

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 
 from config import db ,ma
-
+from marshmallow_sqlalchemy import fields
 
 class Trail(db.Model):
     __tablename__ = "TRAILS"
 
-    Trailid = db.Column(db.Integer, primary_key=True)
+    Trailid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     TrailName = db.Column(db.String(128))
-    TrailOwnerId = db.Column(db.Integer)
+    TrailOwnerId = db.Column(db.Integer, db.ForeignKey("USERS.Userid"))
     TrailElevationgain = db.Column(db.Integer)
     TrailImageFileLocation = db.Column(db.String(512))
     TrailLength = db.Column(db.Float)
@@ -15,7 +15,14 @@ class Trail(db.Model):
     TrailDescription = db.Column(db.String(512))
     TrailStartingPointid = db.Column(db.Integer)
 
+class User(db.Model):
+    __tablename__ = "USERS"
 
+    Userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    Email = db.Column(db.String(128))
+    Password = db.Column(db.String(32))
+    UserPermissionLevel = db.Column(db.Integer)
 
 
 class TrailSchema(ma.SQLAlchemyAutoSchema):
@@ -23,6 +30,7 @@ class TrailSchema(ma.SQLAlchemyAutoSchema):
         model = Trail
         load_instance = True
         sqla_session = db.session
+        include_relationships = True
         
 
 Trail_Schema = TrailSchema()
